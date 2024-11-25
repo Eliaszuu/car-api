527d890fb32c0aa171dc308df2e082186cb1493b
elias <elias.zulauf@lernende.bbw.ch>
2024-11-25 10:41:56 +0100

    fix openapi-spec.yaml

--- src/test/java/ch/bbw/m320/car/controller/CarControllerTest.java
+++ src/test/java/ch/bbw/m320/car/controller/CarControllerTest.java
@@ -1,15 +1,18 @@
-package ch.bbw.m320.car.Controller;
+package ch.bbw.m320.car.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootTest
 class CarControllerTest {
-
+    WebClient webClient;
     //GET
 
     @Test
-    void getAllCar() {}
+    void getAllCar() {
+        
+    }
 
 
 }
